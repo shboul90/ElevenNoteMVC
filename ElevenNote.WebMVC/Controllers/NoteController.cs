54e8d506001fc99e23ec3bbc799b25f2fd325949
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-10-13 22:43:08 -0400

    NoteService.cs added

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,40 @@
+﻿using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{               //first part of our path. (localhost:xxxxx/Note)
+    public class NoteController : Controller
+    {
+        [Authorize]
+        // GET: Note
+               //return type //localhost:xxxxx/Note/Index.
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            //return a view for that path
+            return View(model);
+        }
+
+        // GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            return View(model);
+        }
+    }
+}
