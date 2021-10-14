a72a9b10d1f2ab609585ac3778b6e357945a70ce
Mohamad Al-Shboul <Shboul90@gmail.com>
2021-10-13 23:35:43 -0400

    Edit method added

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -76,5 +76,22 @@ namespace ElevenNote.Services
                     };
             }
         }
+
+        public bool UpdateNote (NoteEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == model.NoteId && e.OwnerId == _userId);
+
+                entity.Title = model.Title;
+                entity.Content = model.Content;
+                entity.ModifiedUtc = DateTimeOffset.Now;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
